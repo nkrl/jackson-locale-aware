1d829b91a78a639c37245f889bf3af072fea7e6a
Niklas <github@nkls.tk>
2023-11-24 22:37:12 +0100

    Clean up

--- src/main/java/nkrl/jackson/l10n/append/LocaleAwareMultiPropertyWriter.java
+++ src/main/java/nkrl/jackson/l10n/append/LocaleAwareMultiPropertyWriter.java
@@ -1,17 +1,17 @@
 package nkrl.jackson.l10n.append;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.PropertyName;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
-import com.fasterxml.jackson.databind.introspect.*;
+import com.fasterxml.jackson.databind.introspect.AnnotatedClass;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 import com.fasterxml.jackson.databind.ser.VirtualBeanPropertyWriter;
 import com.fasterxml.jackson.databind.util.Annotations;
 
-import java.lang.reflect.Method;
 import java.util.Locale;
 
 /**
@@ -38,9 +38,6 @@ public class LocaleAwareMultiPropertyWriter extends VirtualBeanPropertyWriter {
 
     @Override
     protected Object value(Object bean, JsonGenerator json, SerializerProvider prov) throws Exception {
-        /*
-        === ALT ===
-        */
         for (AnnotatedMethod annotatedMethod : localeAwareBean.memberMethods()) {
             if (annotatedMethod.getParameterCount() != 1 || !annotatedMethod.getParameterType(0).hasRawClass(Locale.class)) {
                 // we do not need to check for parameters inheriting from Locale because the class is final
@@ -56,19 +53,12 @@ public class LocaleAwareMultiPropertyWriter extends VirtualBeanPropertyWriter {
                 // property was renamed in annotation
                 key = renamed.getSimpleName();
             }
-            // TODO only != "" if renamed
-            /*if (annotatedMethod.hasAnnotation(JsonProperty.class)) {
-                annotatedMethod.getName()
-            }
-            else if (annotatedMethod.hasAnnotation(JsonGetter.class)) {
-
-            }*/
 
             // remove Java Bean accessor naming conventions if marked with @JsonGetter TODO unless renamed
             if (annotatedMethod.hasAnnotation(JsonGetter.class)) {
-                String potentialKey = prov.getConfig().getAccessorNaming().forPOJO(prov.getConfig(), annotatedClass).findNameForIsGetter(annotatedMethod, key);
+                String potentialKey = prov.getConfig().getAccessorNaming().forPOJO(prov.getConfig(), localeAwareBean).findNameForIsGetter(annotatedMethod, key);
                 if (potentialKey == null) {
-                    potentialKey = prov.getConfig().getAccessorNaming().forPOJO(prov.getConfig(), annotatedClass).findNameForRegularGetter(annotatedMethod, key);
+                    potentialKey = prov.getConfig().getAccessorNaming().forPOJO(prov.getConfig(), localeAwareBean).findNameForRegularGetter(annotatedMethod, key);
                 }
                 if (potentialKey != null) {
                     key = potentialKey;
@@ -85,43 +75,6 @@ public class LocaleAwareMultiPropertyWriter extends VirtualBeanPropertyWriter {
             json.writeFieldName(key);
             prov.findValueSerializer(result.getClass()).serialize(result, json, prov);
         }
-        /*
-        === ORIG ===
-         */
-
-        // call next annotated methods with reflection and return its return value
-        /*for (Method method : bean.getClass().getDeclaredMethods()) {
-            String key;
-            if (method.isAnnotationPresent(JsonProperty.class)) {
-                key = method.getAnnotation(JsonProperty.class).value();
-                if (key.equals(JsonProperty.USE_DEFAULT_NAME)) {
-                    key = method.getName();
-                }
-            }
-            else if (method.isAnnotationPresent(JsonGetter.class)) {
-                key = method.getAnnotation(JsonGetter.class).value();
-                if (key.isEmpty()) {
-                    key = method.getName();
-                }
-                // TODO remove prefix from getSomething isSomething
-
-
-                //prov.getConfig().getPropertyNamingStrategy().nameForGetterMethod(prov.getConfig(), getMember()., key);
-            }
-            else {
-                continue;
-            }
-
-            if (method.getParameterCount() != 1 || !method.getParameterTypes()[0].equals(Locale.class)) {
-                continue;
-            }
-
-            Object result = method.invoke(bean, prov.getLocale());
-            json.writeFieldName(key);
-            prov.findValueSerializer(result.getClass()).serialize(result, json, prov);
-            //json.writeFieldName(key);
-            //json.writeString(result.toString());
-        }*/
 
         /*
          Usually we would have to return the attribute value to serialize here. Note that
@@ -132,8 +85,7 @@ public class LocaleAwareMultiPropertyWriter extends VirtualBeanPropertyWriter {
 
     @Override
     public VirtualBeanPropertyWriter withConfig(MapperConfig<?> config, AnnotatedClass declaringClass, BeanPropertyDefinition propDef, JavaType type) {
-        // declaring class is entity, propDef is nameless SimpleBeanPropertyDefinition with VirtualAnnotatedMember
-
+        // declaring class is locale-aware bean, propDef is nameless SimpleBeanPropertyDefinition with VirtualAnnotatedMember
         return new LocaleAwareMultiPropertyWriter(declaringClass, propDef, declaringClass.getAnnotations(), type);
     }
 }
